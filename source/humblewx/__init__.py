00e5b2d913779c333d44c9a6f34e82ee98000219
Rickard Lindberg <ricli85@gmail.com>
2015-09-30 09:30:53 +0200

    Problem: License mentions Timeline.
    Solution: Change to humblewx.

--- source/humblewx/__init__.py
+++ source/humblewx/__init__.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015  Rickard Lindberg, Roger Lindberg
 #
-# This file is part of Timeline.
+# This file is part of humblewx.
 #
-# Timeline is free software: you can redistribute it and/or modify
+# humblewx is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Timeline is distributed in the hope that it will be useful,
+# humblewx is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+# along with humblewx.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import platform
