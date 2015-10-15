0e03be330aaf13fed8acfcb4bd98551f3a90ff86
Rickard Lindberg <ricli85@gmail.com>
2015-10-15 07:17:54 +0200

    Problem: Can't install in standard Python way.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="humblewx",
+    version="0.1.0",
+    description="Simplifies creating user interfaces with wxPython.",
+    url="https://github.com/thetimelineproj/humblewx",
+    maintainer="Rickard Lindberg",
+    maintainer_email="ricli85@gmail.com",
+    license="GPLv3",
+    packages=["humblewx"],
+    package_dir={"": "source"},
+)
